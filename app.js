4eadc57c119ba47fee0d257a3d5de3f8580482fd
Hiroshi Takaichi <hirosi.t822@gmail.com>
2020-08-01 23:53:13 +0900

    add schedule task

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 const url = require("url");
 // const { totalScore } = require("./totalScore");
 const { totalScoreBatchHandler } = require("./totalScoreBatchHandler");
+const cron = require("node-cron")
 
 http
   .createServer(async (req, res) => {
@@ -15,3 +16,5 @@ http
   .listen(8080, () => {
     console.log("server is now listening htttp://localhost:8080");
   });
+
+cron.schedule('*/3 * * * * *', () => console.log('3秒ごとに実行'));
